b4c060ba007c937907bb7f2a038fa88551b9830d
iugy <eugenio.massella@mail.polimi.it>
2023-02-18 18:33:28 +0100

    bug fix

--- Offline_ipOverWdm_routingSpectrumAndModulationAssignmentHeuristicNotGrooming.java
+++ Offline_ipOverWdm_routingSpectrumAndModulationAssignmentHeuristicNotGrooming.java
@@ -682,8 +682,6 @@ public class Offline_ipOverWdm_routingSpectrumAndModulationAssignmentHeuristicNo
 				tr.setOutputProperty(OutputKeys.INDENT, "yes");
 				tr.setOutputProperty(OutputKeys.METHOD, "xml");
 				tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-				tr.setOutputProperty(OutputKeys.STANDALONE, "yes");
-				//tr.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "roles.dtd");
 				tr.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
 
 				// send DOM to file
